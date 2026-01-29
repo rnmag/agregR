83fc7cd1adb213cca8b7ae72871297f197145cff
Rafael Magalhães <rnunesmagalhaes@gmail.com>
2026-01-29 19:46:28 -0300

    another attempt to crush warnings

--- src/install.libs.R
+++ src/install.libs.R
@@ -27,7 +27,9 @@ fs::dir_copy(path = "stan", new_path = bin_stan)
 callr::r(
   func = function(bin_stan) {
     instantiate::stan_package_compile(
-      models = instantiate::stan_package_model_files(path = bin_stan)
+      models = instantiate::stan_package_model_files(path = bin_stan),
+      quiet = TRUE,
+      verbose = FALSE
     )
   },
   args = list(bin_stan = bin_stan),
